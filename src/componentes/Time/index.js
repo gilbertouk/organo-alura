a3ff66c55bcb4d47bac53bdfddf51acada355a03
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2023-11-25 01:09:07 +0000

    :sparkles: feat: add option to change team color

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,24 +1,31 @@
+import hexToRgba from "hex-to-rgba";
 import Colaborador from "../Colaborador";
 import "./time.css";
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
   return (
     colaboradores.length > 0 && (
       <section
         className="time"
         style={{
           backgroundImage: "url(/imagens/fundo.png)",
-          backgroundColor: time.corPrimaria,
+          backgroundColor: hexToRgba(time.cor, "0.6"),
         }}
       >
-        <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        <input
+          type="color"
+          className="input-cor"
+          value={time.cor}
+          onChange={(evento) => mudarCor(evento.target.value, time.nome)}
+        />
+        <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
         <div className="colaboradores">
           {colaboradores.map((colaborador, indice) => {
             return (
               <Colaborador
                 key={indice}
                 colaborador={colaborador}
-                corDeFundo={time.corSecundaria}
+                corDeFundo={time.cor}
                 aoDeletar={aoDeletar}
               />
             );
